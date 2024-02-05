263644f8b5eb8ec9f5649bfd6e8a3488b0eb3393
Skanx7 <skandergorgi007@gmail.com>
2024-02-05 21:23:16 +0100

    dataHandler class + Preprocess class

--- preprocess.py
+++ preprocess.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import codecs
 
@@ -6,36 +7,56 @@ FILEPATH_TEST_DATA = "test.csv"
 FILEPATH_RESULTS_DATA = "results.csv"
 
 class DataHandler:
+    """Class to handle the reading and writing of data to and from a file."""
     def __init__(self, fp : str = FILEPATH_TRAIN_DATA):
-        self.df : pd.DataFrame = None
+        """Initializes the DataHandler object with the file path to the data file."""
         self.fp : str = fp
-    def read(self, fp : str = FILEPATH_TRAIN_DATA) -> pd.DataFrame:
-        data = pd.read_csv(fp, sep = ",",encoding="UTF-8")
+        self.df : pd.DataFrame = None
+        self.read()
+    def read(self) -> None:
+        """Reads the data from the file and stores it in the df attribute."""
+        data = pd.read_csv(self.fp, sep = ",",encoding="UTF-8")
         df = pd.DataFrame(data)
         self.df = df
     def write(self, df : pd.DataFrame) -> None:
+        """Writes the data to the file from the df attribute."""
         f = codecs.open(self.fp, mode='w', encoding='utf-8')
         f.write(df.to_string())
         f.close()
+
+train_data = DataHandler(FILEPATH_TRAIN_DATA).df
+test_data = DataHandler(FILEPATH_TEST_DATA).df
+
+
+results = DataHandler(FILEPATH_RESULTS_DATA)
+
 class PreProcess:
-    def __init__(self, dh: DataHandler):
-        self.dh = dh
-        
+    """Class to preprocess the data for the Spaceship Titanic dataset."""
+    def __init__(self, df: pd.DataFrame):
+        self.redundant_cols_bool = True
+        self.df : pd.DataFrame = df
+    def set_index(self) -> None:
+        self.df.set_index("PassengerId")
+    def categorize_bools(self) -> None:
+        """Categorizes the boolean columns into 0, 1, -1 for False, True, and NaN respectively."""
+        bool_columns = ['CryoSleep', 'VIP', 'Transported']
+        self.df[bool_columns] = self.df[bool_columns].apply(lambda x: x.map({True: 1, False: 0, np.nan: -1}))
+    def add_columns(self) -> None:
+        """Adds new columns to the dataframe."""
+        self.df[['GroupId', 'NumberId']] = self.df['PassengerId'].str.split('_', expand=True)                   # Split the PassengerId into two columns
+        self.df[['CabinDeck', 'CabinNumber', 'CabinSide']] = self.df['Cabin'].str.split('/', expand=True)       # Split the Cabin into three columns
+        excluded_cols = [] if self.redundant_cols_bool else ['PassengerId', 'Cabin']
+        cols = ['GroupId', 'NumberId', 'CabinDeck', 'CabinNumber', 'CabinSide'] +\
+            [col for col in self.df.columns if col not in ['GroupId', 'NumberId', 'CabinDeck', 'CabinNumber', 'CabinSide'] and col not in excluded_cols]
+        self.df = self.df[cols]
+    def run(self) -> None:
+        """Runs the preprocessing steps."""
+        self.set_index()
+        self.categorize_bools()
+        self.add_columns()
 
-def write_data(df: pd.DataFrame, fp : str = FILEPATH_RESULTS_DATA) -> None:
-    f = codecs.open(fp, mode='w', encoding='utf-8')
-    f.write(df.to_string())
-    f.close()
-def set_index(df: pd.DataFrame, fp: str = FILEPATH_TRAIN_DATA) -> None:
-    df.set_index("PassengerId")
-def add_columns(df: pd.DataFrame) -> None:
-    #We seperate the group and number in the group and the number in the group into gggg and pp columns instead of gggg_pp which are unique
-    passenger_ids : list = df.loc[:, 'PassengerId']
-    splitted_passenger_ids = [passenger_ids[i].split('_') for i in range(len(passenger_ids))]
-    group_ids = map(lambda c : c[0], splitted_passenger_ids)
-    number_ids = map(lambda c : c[0], splitted_passenger_ids)
-    df.insert(0,group_ids)
+preprocess = PreProcess(train_data)
+preprocess.run()
+preprocessed_train_data = preprocess.df
+results.write(preprocessed_train_data)
 
-df = read_data()
-add_columns(df)
-write_data(df)
